e82aa28eb61b3d527d639ff7ac59e2c7ed4a35a6
Szász Tibor <tszasz@gmail.com>
2017-07-11 17:14:09 +0200

    fix(sworkers): fixed paths

--- sworker.js
+++ sworker.js
@@ -4,14 +4,14 @@ importScripts('sw-toolbox.js');
 'use strict';
 
 toolbox.precache([
-	'/index.html', 
-	'/css/site.css', 
-	'/app.js',
-	'/clipboard.min.js',
-	'/assets/green_fox_logo.png',
-	'/assets/apocolypse.svg',
-	'/assets/close-icon.svg',
-	'/assets/list-icon.svg',
+	'index.html', 
+	'css/site.css', 
+	'app.js',
+	'clipboard.min.js',
+	'assets/green_fox_logo.png',
+	'assets/apocolypse.svg',
+	'assets/close-icon.svg',
+	'assets/list-icon.svg',
 ]);
 
 toolbox.router.default = toolbox.cacheFirst;
