538024f14ef608fd4c13fa4e3b129005ae4e5f7d
Szász Tibor <tszasz@gmail.com>
2017-07-11 17:01:17 +0200

    added files to cache

--- sworker.js
+++ sworker.js
@@ -3,7 +3,16 @@ importScripts('node_modules/sw-toolbox/sw-toolbox.js');
 (function(global) {
 'use strict';
 
-toolbox.precache(['/index.html', '/site.css']);
+toolbox.precache([
+	'/index.html', 
+	'/css/site.css', 
+	'/app.js',
+	'/clipboard.min.js',
+	'/assets/green_fox_logo.png',
+	'/assets/apocolypse.svg',
+	'/assets/close-icon.svg',
+	'/assets/list-icon.svg',
+]);
 
 toolbox.router.default = toolbox.cacheFirst;
 
